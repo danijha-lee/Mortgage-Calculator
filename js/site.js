ab346b24f362fa6b51e464d207a4a14d014e5d32
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-07-21 23:34:33 -0400

    updated html. some js

--- js/site.js
+++ js/site.js
@@ -1,57 +1,33 @@
-fucntion getValues(){
-    let fizzValue = document.getElementById("fizzValue").value;
-    let buzzValue = document.getElementById("buzzValue").value;
+let amount = document.getElementById("amount").value;
+let term = document.getElementById("term").value;
+let rate = document.getElementById("rate").value;
 
-    fizzValue = parseInt(fizzValue);
-    buzzValue = parseInt(buzzValue);
-
-    if (Number.isInteger(fizzvalue) && Number.isInteger(buzzValue)) {
-
-        let fbArray = FizzBuzz(fizzValue, buzzValue);
-        displayData(fbArray);
-    }else{
-        alert("You must enter integers")
-    }
+function calcPayment(amount, term, rate) {
+    return (amount * rate) / (1 - Math.pow(1 + rate, - term));
 }
 
-function fizzBuzz(fizzValue, buzzValue)
-{
-    let returnArray = [];
-
-    for (let index = 1; index < 100; index++) {
-        
-        if (index % fizzValue == 0 && index % buzzValue) {
-            returnArray.push('FizzBuzz');
-        } else if{
-            returnArray.push('Fizz');
-        }else if (index % buzzValue == 0){
-            returnArray.push('Buzz');
-        }else {
-            returnArray.push(index);
-        }
-        
-        returnArray.push(index);
-        
-    }
+function calcInterest(balance, rate) {
+    return balance * rate;
 }
 
-function displayData(fbArray) {
-    let tableBody = document.getElementById('results');
-
-    let templateRow = document.getElementById('fbTemplate');
-    
-    tableBody.innerHTML = "";
-
-    for (let index = 0; index < fbArray.length; index+=5) {
-        let tableRow = document.importNode(templateRow.content, true);
-
-        let rowCols = tableRow.querySelectorAll('td');
-        rowCols[0].textContent = fbData[i];
-        rowCols[1].textContent = fbData[i+1];
-        rowCols[2].textContent = fbData[i+2];
-        rowCols[3].textContent = fbData[i+3];
-        rowCols[4].textContent = fbData[i + 4];
-        
-        tableBody.appendChild(tableRow);
-    }
+function calcRate() {
+    let rate = parseFloat(document.getElementById("rate").value);
+    return rate = rate / 1200;
 }
+
+function buildSchedule() {
+    let loanAmount = Number(document.getElementById("lamount").value);
+    let term = parseInt(document.getElementById("lterm").value);
+    let rate = calcRate();
+
+    let payment = calcPayment(loanAmount, rate, term);
+    let payments = getPayments(loanAmount, rate, term, payment);
+
+    displayData(payments, loanAmount, payment);
+}
+
+function getPayments(amount, rate, term, payment) {
+    let payments = [];
+}
+
+let balance = amount
